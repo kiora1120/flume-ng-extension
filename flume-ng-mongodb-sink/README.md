10b362aef23ba58d537b34584985d97d15d0198a
jhshin9 <jhshin9@gmail.com>
2012-12-05 15:20:14 +0900

    first commit and push

---
+++ flume-ng-mongodb-sink/README.md
@@ -0,0 +1,57 @@
+flume-ng-mongodb-sink
+=============
+Flume NG MongoDB sink. The source was implemented to populate JSON into MongoDB.
+
+## Getting Started
+- - -
+1. Clone the repository
+2. Install latest Maven and build source by 'mvn package'
+3. Generate classpath by 'mvn dependency:build-classpath'
+4. Append classpath in $FLUME_HOME/conf/flume-env.sh
+5. Add the sink definition according to **Configuration**
+
+## Configuration
+- - - 
+	type: MongoSink
+	host: db host [localhost]
+	port: db port [27017]
+	username: db username []
+	password: db password []
+	model: single or dynamic, single mean all data will insert into the same collection,
+	    and dynamic means every event will specify cllection name by event header 'collection' [single]
+	db: db name [events]
+	collection: default collection name, will used in single model [events]
+	batch: batch size of insert opertion [100]
+	autoWrap: indicator of wrap the event body as a JSONObject that has one field [false]
+	wrapField: use with autoWrap, set the field name of JSONObject [log]
+
+### flume.conf sample
+- - -
+agent2.sources = source2
+agent2.channels = channel2
+agent2.sinks = sink2
+
+agent2.sources.source2.type = org.riderzen.flume.source.MsgPackSource
+agent2.sources.source2.bind = localhost
+agent2.sources.source2.port = 1985
+
+agent2.sources.source2.channels = channel2
+
+agent2.sinks.sink2.type = MongoSink
+agent2.sinks.sink2.host = localhost
+agent2.sinks.sink2.port = 27017
+agent2.sinks.sink2.model = single
+agent2.sinks.sink2.collection = events
+agent2.sinks.sink2.batch = 100
+
+agent2.sinks.sink2.channel = channel2
+
+agent2.channels.channel2.type = memory
+agent2.channels.channel2.capacity = 1000000
+agent2.channels.channel2.transactionCapacity = 800
+agent2.channels.channel2.keep-alive = 3
+
+### Event Headers
+    The sink supports some headers in dynamic model:
+    'db': db name
+    'collection' : collection name
---
+++ flume-ng-mongodb-sink/src/test/java/MongoSinkTest.java
@@ -0,0 +1,317 @@
+import com.mongodb.*;
+import org.apache.commons.collections.MapUtils;
+import org.apache.flume.*;
+import org.apache.flume.channel.MemoryChannel;
+import org.apache.flume.conf.Configurables;
+import org.apache.flume.event.EventBuilder;
+import org.json.simple.JSONObject;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.UnknownHostException;
+import java.util.*;
+
+import static org.testng.Assert.*;
+
+/**
+ * User: guoqiang.li
+ * Date: 12-9-12
+ * Time: 下午3:31
+ */
+@Test(singleThreaded = true, threadPoolSize = 1)
+public class MongoSinkTest {
+	private static Mongo mongo;
+	public static final String DBNAME = "myDb";
+
+	private static Context ctx = new Context();
+	private static Channel channel;
+
+
+	@BeforeClass(groups = {"dev"})
+	public static void setup() throws UnknownHostException {
+		mongo = new Mongo("localhost", 27017);
+
+		Map<String, String> ctxMap = new HashMap<String, String>();
+		ctxMap.put(MongoSink.HOST, "localhost");
+		ctxMap.put(MongoSink.PORT, "27017");
+		ctxMap.put(MongoSink.DB_NAME, "test_events");
+		ctxMap.put(MongoSink.COLLECTION, "test_log");
+		ctxMap.put(MongoSink.BATCH_SIZE, "100");
+
+		ctx.putAll(ctxMap);
+
+		Context channelCtx = new Context();
+		channelCtx.put("capacity", "1000000");
+		channelCtx.put("transactionCapacity", "1000000");
+		channel = new MemoryChannel();
+		Configurables.configure(channel, channelCtx);
+	}
+
+	@AfterClass(groups = {"dev"})
+	public static void tearDown() {
+		mongo.close();
+	}
+
+	@AfterMethod(groups = {"dev"})
+	public static void cleanDb() {
+		mongo.dropDatabase(DBNAME);
+		mongo.dropDatabase("test_events");
+		mongo.dropDatabase("dynamic_db");
+
+	}
+
+	@Test(groups = "dev", invocationCount = 1)
+	public void sinkDynamicTest() throws EventDeliveryException, InterruptedException {
+		ctx.put(MongoSink.MODEL, MongoSink.CollectionModel.dynamic.name());
+		MongoSink sink = new MongoSink();
+		Configurables.configure(sink, ctx);
+
+		sink.setChannel(channel);
+		sink.start();
+
+		JSONObject msg = new JSONObject();
+		msg.put("age", 11);
+		msg.put("birthday", new Date().getTime());
+
+		Transaction tx;
+
+		for (int i = 0; i < 100; i++) {
+			tx = channel.getTransaction();
+			tx.begin();
+			msg.put("name", "test" + i);
+			JSONObject header = new JSONObject();
+			header.put(MongoSink.COLLECTION, "my_events");
+
+			Event e = EventBuilder.withBody(msg.toJSONString().getBytes(), header);
+			channel.put(e);
+			tx.commit();
+			tx.close();
+		}
+		sink.process();
+		sink.stop();
+
+		for (int i = 0; i < 10; i++) {
+			msg.put("name", "test" + i);
+
+			System.out.println("i = " + i);
+
+			DB db = mongo.getDB("test_events");
+			DBCollection collection = db.getCollection("my_events");
+			DBCursor cursor = collection.find(new BasicDBObject(msg));
+			assertTrue(cursor.hasNext());
+			DBObject dbObject = cursor.next();
+			assertNotNull(dbObject);
+			assertEquals(dbObject.get("name"), msg.get("name"));
+			assertEquals(dbObject.get("age"), msg.get("age"));
+			assertEquals(dbObject.get("birthday"), msg.get("birthday"));
+		}
+	}
+
+	@Test(groups = "dev")
+	public void sinkDynamicTest2() throws EventDeliveryException, InterruptedException {
+		ctx.put(MongoSink.MODEL, MongoSink.CollectionModel.dynamic.name());
+		MongoSink sink = new MongoSink();
+		Configurables.configure(sink, ctx);
+
+		sink.setChannel(channel);
+		sink.start();
+
+		JSONObject msg = new JSONObject();
+		msg.put("age", 11);
+		msg.put("birthday", new Date().getTime());
+
+		Transaction tx;
+
+		for (int i = 0; i < 100; i++) {
+			tx = channel.getTransaction();
+			tx.begin();
+			msg.put("name", "test" + i);
+			JSONObject header = new JSONObject();
+			header.put(MongoSink.COLLECTION, "my_events" + i % 10);
+
+			Event e = EventBuilder.withBody(msg.toJSONString().getBytes(), header);
+			channel.put(e);
+			tx.commit();
+			tx.close();
+		}
+		sink.process();
+		sink.stop();
+
+		for (int i = 0; i < 10; i++) {
+			msg.put("name", "test" + i);
+
+			System.out.println("i = " + i);
+
+			DB db = mongo.getDB("test_events");
+			DBCollection collection = db.getCollection("my_events" + i % 10);
+			DBCursor cursor = collection.find(new BasicDBObject(msg));
+			assertTrue(cursor.hasNext());
+			DBObject dbObject = cursor.next();
+			assertNotNull(dbObject);
+			assertEquals(dbObject.get("name"), msg.get("name"));
+			assertEquals(dbObject.get("age"), msg.get("age"));
+			assertEquals(dbObject.get("birthday"), msg.get("birthday"));
+		}
+	}
+
+	@Test(groups = "dev")
+	public void sinkSingleModelTest() throws EventDeliveryException {
+		ctx.put(MongoSink.MODEL, MongoSink.CollectionModel.single.name());
+
+		MongoSink sink = new MongoSink();
+		Configurables.configure(sink, ctx);
+
+		sink.setChannel(channel);
+		sink.start();
+
+		Transaction tx = channel.getTransaction();
+		tx.begin();
+		JSONObject msg = new JSONObject();
+		msg.put("name", "test");
+		msg.put("age", 11);
+		msg.put("birthday", new Date().getTime());
+
+		Event e = EventBuilder.withBody(msg.toJSONString().getBytes());
+		channel.put(e);
+		tx.commit();
+		tx.close();
+
+		sink.process();
+		sink.stop();
+
+		DB db = mongo.getDB("test_events");
+		DBCollection collection = db.getCollection("test_log");
+		DBCursor cursor = collection.find(new BasicDBObject(msg));
+		assertTrue(cursor.hasNext());
+		DBObject dbObject = cursor.next();
+		assertNotNull(dbObject);
+		assertEquals(dbObject.get("name"), msg.get("name"));
+		assertEquals(dbObject.get("age"), msg.get("age"));
+		assertEquals(dbObject.get("birthday"), msg.get("birthday"));
+	}
+
+	@Test(groups = "dev")
+	public void dbTest() {
+		DB db = mongo.getDB(DBNAME);
+		db.getCollectionNames();
+		List<String> names = mongo.getDatabaseNames();
+
+		assertNotNull(names);
+		boolean hit = false;
+
+		for (String name : names) {
+			if (DBNAME.equals(name)) {
+				hit = true;
+				break;
+			}
+		}
+
+		assertTrue(hit);
+	}
+
+	@Test(groups = "dev")
+	public void collectionTest() {
+		DB db = mongo.getDB(DBNAME);
+		DBCollection myCollection = db.getCollection("myCollection");
+		myCollection.save(new BasicDBObject(MapUtils.putAll(new HashMap(), new Object[]{"name", "leon", "age", 33})));
+		myCollection.findOne();
+
+		Set<String> names = db.getCollectionNames();
+
+		assertNotNull(names);
+		boolean hit = false;
+
+		for (String name : names) {
+			if ("myCollection".equals(name)) {
+				hit = true;
+				break;
+			}
+		}
+
+		assertTrue(hit);
+	}
+
+	//    @Test(groups = "dev")
+	public void autoWrapTest() throws EventDeliveryException {
+		ctx.put(MongoSink.AUTO_WRAP, Boolean.toString(true));
+		ctx.put(MongoSink.DB_NAME, "test_wrap");
+
+		MongoSink sink = new MongoSink();
+		Configurables.configure(sink, ctx);
+
+		sink.setChannel(channel);
+		sink.start();
+
+		Transaction tx = channel.getTransaction();
+		tx.begin();
+		String msg = "2012/10/26 11:23:08 [error] 7289#0: *6430831 open() \"/usr/local/nginx/html/50x.html\" failed (2: No such file or directory), client: 10.160.105.161, server: sg15.redatoms.com, request: \"POST /mojo/ajax/embed HTTP/1.0\", upstream: \"fastcgi://unix:/tmp/php-fpm.sock:\", host: \"sg15.redatoms.com\", referrer: \"http://sg15.redatoms.com/mojo/mobile/package\"";
+
+		Event e = EventBuilder.withBody(msg.getBytes());
+		channel.put(e);
+		tx.commit();
+		tx.close();
+
+		sink.process();
+		sink.stop();
+
+		DB db = mongo.getDB("test_wrap");
+		DBCollection collection = db.getCollection("test_log");
+		DBCursor cursor = collection.find(new BasicDBObject(MongoSink.DEFAULT_WRAP_FIELD, msg));
+		assertTrue(cursor.hasNext());
+		DBObject dbObject = cursor.next();
+		assertNotNull(dbObject);
+		assertEquals(dbObject.get(MongoSink.DEFAULT_WRAP_FIELD), msg);
+		mongo.dropDatabase("test_wrap");
+	}
+
+	@Test(groups = "dev")
+	public void sinkDynamicDbTest() throws EventDeliveryException {
+		ctx.put(MongoSink.MODEL, MongoSink.CollectionModel.dynamic.name());
+		MongoSink sink = new MongoSink();
+		Configurables.configure(sink, ctx);
+
+		sink.setChannel(channel);
+		sink.start();
+
+		JSONObject msg = new JSONObject();
+		msg.put("age", 11);
+		msg.put("birthday", new Date().getTime());
+
+		Transaction tx;
+
+		for (int i = 0; i < 10; i++) {
+			tx = channel.getTransaction();
+			tx.begin();
+			msg.put("name", "test" + i);
+			JSONObject header = new JSONObject();
+			header.put(MongoSink.COLLECTION, "my_events");
+			header.put(MongoSink.DB_NAME, "dynamic_db");
+
+			Event e = EventBuilder.withBody(msg.toJSONString().getBytes(), header);
+			channel.put(e);
+			tx.commit();
+			tx.close();
+		}
+		sink.process();
+		sink.stop();
+
+		for (int i = 0; i < 10; i++) {
+			msg.put("name", "test" + i);
+
+			System.out.println("i = " + i);
+
+			DB db = mongo.getDB("dynamic_db");
+			DBCollection collection = db.getCollection("my_events");
+			DBCursor cursor = collection.find(new BasicDBObject(msg));
+			assertTrue(cursor.hasNext());
+			DBObject dbObject = cursor.next();
+			assertNotNull(dbObject);
+			assertEquals(dbObject.get("name"), msg.get("name"));
+			assertEquals(dbObject.get("age"), msg.get("age"));
+			assertEquals(dbObject.get("birthday"), msg.get("birthday"));
+		}
+
+	}
+}
