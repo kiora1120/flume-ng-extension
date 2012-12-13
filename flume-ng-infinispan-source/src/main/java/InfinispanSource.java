86be86ca4cd57f8fb0a2245e5c68ac32e78b99c2
jhshin9 <jhshin9@gmail.com>
2012-12-13 09:48:25 +0900

    first commit

---
+++ flume-ng-infinispan-source/src/main/java/InfinispanSource.java
@@ -0,0 +1,20 @@
+import org.apache.flume.Context;
+import org.apache.flume.EventDrivenSource;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.source.AbstractSource;
+
+/**
+ * Infinispan Source.
+ *
+ * </p>
+ *
+ * @author <a href="mailto:jhshin9@gmail.com">신정훈</a>
+ * @since 1.0
+ */
+public class InfinispanSource extends AbstractSource implements Configurable, EventDrivenSource {
+
+	@Override
+	public void configure(Context context) {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+}
