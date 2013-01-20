461597515e25a61825d5ea47e16385a062e0f4e4
tedwon <iamtedwon@gmail.com>
2013-01-20 18:37:49 +0900

    Add new sink classes

---
+++ flume-ng-custom-sources/src/main/java/com/tedwon/KafkaSink.java
@@ -0,0 +1,68 @@
+package com.tedwon;
+
+import org.apache.flume.Channel;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.Transaction;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.event.EventHelper;
+import org.apache.flume.sink.AbstractSink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Kafka Flume Sink Class.
+ * <p/>
+ *
+ * @author <a href="iamtedwon@gmail.com">Ted Won</a>
+ * @version 1.0
+ */
+public class KafkaSink extends AbstractSink implements Configurable {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaSink.class);
+
+
+    @Override
+    public void configure(Context context) {
+    }
+
+    @Override
+    public Status process() throws EventDeliveryException {
+        Status status = Status.READY;
+        Channel channel = getChannel();
+        Transaction tx = null;
+        try {
+            tx = channel.getTransaction();
+            tx.begin();
+
+            Event event = channel.take();
+
+            if (event != null) {
+                String line = EventHelper.dumpEvent(event);
+                logger.debug(line);
+
+                byte[] body = event.getBody();
+                String data = new String(body);
+
+                logger.debug(data);
+            } else {
+                status = Status.BACKOFF;
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            logger.error("can't process events, drop it!", e);
+            if (tx != null) {
+                tx.commit();// commit to drop bad event, otherwise it will enter dead loop.
+            }
+
+            throw new EventDeliveryException(e);
+        } finally {
+            if (tx != null) {
+                tx.close();
+            }
+        }
+        return status;
+    }
+}
