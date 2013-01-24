47835521c5ab0a75f0a9f11b90692897c2df178f
jhshin9 <jhshin9@gmail.com>
2013-01-24 16:39:02 +0900

    1차 완성.

--- flume-ng-infinispan-sink/src/main/java/InfinispanSink.java
+++ flume-ng-infinispan-sink/src/main/java/InfinispanSink.java
@@ -1,16 +1,94 @@
+import org.apache.flume.*;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.sink.AbstractSink;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Description.
+ * Jboss Infinispan Sink.
  * </p>
+ * <p/>
+ * Put data into Infinispan Data Grid.
  *
- * @author <a href="mailto:jhshin9@gmail.com">신정훈</a>
+ * @author <a href="mailto:jhshin9@gmail.com">Junghun, Shin (Korea)</a>
  * @version 1.0
  * @since 1.0
  */
-public class InfinispanSink {
+public class InfinispanSink extends AbstractSink implements Configurable {
 
+    /**
+     * SLF4J Logging
+     */
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public static void main(String[] args) {
+    private String nodes;
 
+    private String cacheName;
+
+    RemoteCacheManager remoteCacheManager;
+
+    RemoteCache remoteCache;
+
+    @Override
+    public void configure(Context context) {
+        nodes = context.getString("nodes", "localhost:11222");
+        cacheName = context.getString("cacheName", "localCache");
     }
 
+    @Override
+    public synchronized void start() {
+        logger.info("InfinispanSink is started.............");
+        remoteCacheManager = new RemoteCacheManager(nodes);
+        remoteCache = remoteCacheManager.getCache(cacheName);
+        super.start();
+    }
+
+    @Override
+    public Status process() throws EventDeliveryException {
+
+        Status status = Status.READY;
+        Channel channel = getChannel();
+        Transaction tx = null;
+
+        try {
+            tx = channel.getTransaction();
+            tx.begin();
+
+            Event event = channel.take();
+
+            if (event != null) {
+                byte[] body = event.getBody();
+                String data = new String(body);
+
+                // Put the Event into Infinispan Cache
+                remoteCache.put(event.hashCode(), data);
+
+            } else {
+                status = Status.BACKOFF;
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            logger.error("can't process events, drop it!", e);
+            if (tx != null) {
+                tx.commit();
+            }
+            throw new EventDeliveryException(e);
+        } finally {
+            if (tx != null) {
+                tx.close();
+            }
+        }
+        return status;
+    }
+
+    @Override
+    public synchronized void stop() {
+        logger.info("InfinispanSink is stopped.............");
+        remoteCache.stop();
+        remoteCacheManager.stop();
+        super.stop();
+    }
 }
---
+++ flume-ng-infinispan-sink/src/test/java/InfinispanClientTest.java
@@ -0,0 +1,86 @@
+import junit.framework.Assert;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Infinispan Client Test Case.
+ * </p>
+ *
+ * @author <a href="mailto:jhshin9@gmail.com">Junghun, Shin (Korea)</a>
+ * @version 1.0
+ * @since 1.0
+ */
+public class InfinispanClientTest {
+
+    /**
+     * SLF4J Logging
+     */
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    RemoteCacheManager rcm;
+
+    RemoteCache cache;
+
+    @Before
+    public void setUp() {
+        rcm = new RemoteCacheManager("localhost:11222");
+        cache = rcm.getCache("sharedCache");
+    }
+
+    @Test
+    public void putTest() {
+
+        for (int i = 0; i < 100; i++) {
+            cache.put(i, i);
+        }
+
+        Assert.assertEquals(100, cache.size());
+    }
+
+    @Test
+    public void getTest() {
+
+        for (int i = 0; i < cache.size(); i++) {
+            logger.debug(cache.get(i).toString());
+        }
+
+        Assert.assertEquals(100, cache.size());
+
+    }
+
+    @Test
+    public void getCacheAll(){
+        Map bulk = cache.getBulk();
+
+        Iterator iterator = bulk.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Object next = iterator.next();
+
+            System.out.println(next.toString());
+
+        }
+    }
+
+    @Test
+    public void deleteTest() {
+        cache.clear();
+    }
+
+    @Test
+    public void test() {
+        logger.debug("Cache Name : [{}]", cache.getName());
+        logger.debug("Contains Key : [{}]", cache.containsKey(1));
+        logger.debug("Cache Map : [{}]", cache);
+
+        System.out.println(cache.getBulk());
+    }
+}
+
